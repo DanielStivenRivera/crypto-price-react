74a397b4b0b533ae29557ba0c344605e6aecc229
DanielStivenRivera <danestiv0610@gmail.com>
2021-08-17 00:53:35 -0500

    pagination addded

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ function App() {
 
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState('');
+  const [page, setPage] = useState(1);
 
   const getData = async () => {
-    const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1');
+    const res = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=${page}`);
     setCoins(res.data);
   }
+
+
 
   useEffect(() => {
     getData()
@@ -22,6 +25,14 @@ function App() {
       <div className="row">
         <input type="text" placeholder="search coin" className="form-control bg-dark text-light border-0 mt-4 text-center"
         onChange={e => setSearch(e.target.value)} />
+        <div>
+          <label>Page number</label>
+          <button onClick={e => setPage(e.target.value)}>1</button>
+          <button onClick={e => setPage(e.target.value)}>2</button>
+          <button onClick={e => setPage(e.target.value)}>3</button>
+          <button onClick={e => setPage(e.target.value)}>4</button>
+          <button onClick={e => setPage(e.target.value)}>5</button>
+        </div>
         <TableCoins coins={coins} search={search}/>
       </div>
       <h3>Powered by Daniel Stiven Rivera</h3>
