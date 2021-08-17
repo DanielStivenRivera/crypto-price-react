ff126fc0a7f436b448a888a19bf00ea204a04e03
DanielStivenRivera <danestiv0610@gmail.com>
2021-08-17 01:17:18 -0500

    fixing problems

--- src/App.js
+++ src/App.js
@@ -7,18 +7,11 @@ function App() {
 
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState('');
-<<<<<<< HEAD
   const [page, setPahe] = useState(1);
   //Some chage can't be created
 
   const getData = async () => {
     const res = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1`);
-=======
-  const [page, setPage] = useState(1);
-
-  const getData = async () => {
-    const res = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=${page}`);
->>>>>>> 74a397b4b0b533ae29557ba0c344605e6aecc229
     setCoins(res.data);
   }
 
