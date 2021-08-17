975970f88880d3de6627f5c3b6ef0ce0b1224d00
DanielStivenRivera <danestiv0610@gmail.com>
2021-08-17 01:10:52 -0500

    last build return

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState('');
 
+  
+
   const getData = async () => {
     const res = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1`);
     setCoins(res.data);
